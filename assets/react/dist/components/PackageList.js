985807b4b8c25670c138ef390f09de3767ad68d9
Hugo Alliaume <hugo@alliau.me>
2026-05-12 08:54:50 +0200

    [TailwindCSS] Minor improvements Translator, React and Vue packages

--- assets/react/dist/components/PackageList.js
+++ assets/react/dist/components/PackageList.js
@@ -4,20 +4,22 @@ export default function (props) {
     return 'No packages found';
   }
   return /*#__PURE__*/React.createElement("div", {
-    className: "PackageList"
+    className: "pt-3 grid gap-2 grid-cols-[repeat(auto-fill,minmax(260px,1fr))]"
   }, props.packages.map(item => /*#__PURE__*/React.createElement("div", {
-    className: "PackageListItem",
-    key: item.id
-  }, /*#__PURE__*/React.createElement("div", {
-    className: "PackageListItem__icon",
+    className: "group relative flex gap-6 p-2 self-stretch border border-surface rounded-md bg-body",
+    key: item.id,
     style: {
-      '--gradient': item.gradient
+      '--color': item.color,
+      '--gradient': item.gradient || 'none',
+      '--logo-size': '48px'
     }
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "flex items-center justify-center shrink-0 rounded-[14%] [background-image:var(--gradient)] bg-(--color) w-(--logo-size) h-(--logo-size) [&_img]:w-[calc(0.4*var(--logo-size))] [&_img]:h-auto [&_img]:filter-[drop-shadow(4px_4px_4px_rgba(0,0,0,0))]"
   }, /*#__PURE__*/React.createElement("img", {
     src: item.imageUrl,
     alt: `Image for the ${item.humanName} UX package`
   })), /*#__PURE__*/React.createElement("h3", {
-    className: "PackageListItem__label"
+    className: "font-title font-semibold text-xl leading-normal text-body-text m-0"
   }, /*#__PURE__*/React.createElement("a", {
     href: item.url
   }, item.humanName)))));
