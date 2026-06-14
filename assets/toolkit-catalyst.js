c6a064d0e6561b9071d63b2897fe1b8150e43492
seb-jean <sebastien.jean76@gmail.com>
2026-06-14 15:42:31 +0200

    Add the Catalyst UI kit (by Tailwind Plus) to ux.symfony.com

---
+++ assets/toolkit-catalyst.js
@@ -0,0 +1,7 @@
+import './styles/toolkit-catalyst.css';
+import '@fontsource/inter/latin.css';
+import '@fontsource/inter/latin-italic.css';
+import '@tailwindplus/elements';
+import { startStimulusApp } from '@symfony/stimulus-bundle';
+
+const app = startStimulusApp();
